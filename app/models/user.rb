2abeeee15d0431dacd45c65ea7f9ff3e452aa211
sample33 <sls.49422@gmail.com>
2021-04-14 17:21:17 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,8 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :nickname, :birthday
-    validates :password,         format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+{6}\z/}
-    validates :family_name,      format: {with: /\A[ぁ-んァ-ン一-龥]/ } 
-    validates :family_name_kana, format: {with: /\A[ァ-ヶー－]+\z/ } 
-    validates :first_name,       format: {with: /\A[ぁ-んァ-ン一-龥]/ } 
-    validates :first_name_kana,  format: {with: /\A[ァ-ヶー－]+\z/ } 
+    validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+{6}\z/}
+    validates :family_name, :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
+    validates :family_name_kana, :first_name_kana, format: {with: /\A[ァ-ヶー]+\z/}
   end
 end
